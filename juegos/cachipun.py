f334ea656c78ca44cbd8fb0d0fd5a24f341f4a30
ssya333 <163364989+ssya333@users.noreply.github.com>
2024-03-14 20:30:49 +0000

    Update dado

--- juegos/cachipun.py
+++ juegos/cachipun.py
@@ -1,7 +1,26 @@
+import random
 def cachipun():
-    """
-    Esta función representa el juego de cachipun.
-    Debes pedir al usuario que elija piedra, papel o tijera, y luego comparar su elección con la de la computadora.
-    La computadora debe elegir una opción al azar.
-    """
+    print('Elige piedra, papel o tijera')
+    elecusu = input()
+    opciones = ['piedra', 'papel', 'tijera']
+    num = random.randint(0,2)
+    eleccompu = opciones[num]
+    mensaje = ''
+    if eleccompu == elecusu:
+        mensaje  = 'Oh oh, parece que han empatado, intentalo de nuevo'
+    elif eleccompu == 'tijera' and elecusu == 'piedra':
+        mensaje = 'Yeii, ganaste!!'
+    elif eleccompu == 'tijera' and elecusu == 'papel':
+        mensaje = 'Nooo, perdiste:('
+    elif eleccompu == 'piedra' and elecusu == 'papel':
+        mensaje = 'Yeii, ganaste!!'
+    elif eleccompu == 'piedra' and elecusu == 'tijera':
+        mensaje = 'Nooo, perdiste:('
+    elif eleccompu == 'papel' and elecusu == 'tijera':
+        mensaje = 'Yeii, ganaste!!'
+    elif eleccompu == 'papel' and elecusu == 'piedra':
+        mensaje = 'Nooo, perdiste:('
+    return mensaje
     pass
+
+print(cachipun())
--- juegos/juego_del_dado.py
+++ juegos/juego_del_dado.py
@@ -1,14 +1,19 @@
 import random
 def juego_del_dado():
     puntusu = 0
-    puntcompu = 0
+    puntcomp = 0
     while puntusu <= 30 and puntcomp <= 30:
-        print('Presiona enter para lanzar el dado')
         num1 = random.randint(1,6)
-        puntusua += num1
+        puntusu += num1
+        mensaje = ''
+        if puntusu >= 30:
+            mensaje = 'Ganasteee!!'
+            break
         num2 = random.randint(1,6)
         puntcomp += num2
-        if puntusu >= 30 or puntcompu >= 30:
+        if puntcomp >= 30:
+            mensaje = 'Noo, perdiste:(('
             break
-
+    return mensaje
     pass
+print(juego_del_dado())
