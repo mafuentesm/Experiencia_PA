e25262c48ce177713c2ca768e44add9344ae3a71
Matías Fuentes <163365018+mafuentesm@users.noreply.github.com>
2024-03-15 00:15:59 +0000

    puse unos comentarios que aclaran cuando sacó cada uno

--- juegos/juego_del_dado.py
+++ juegos/juego_del_dado.py
@@ -3,16 +3,20 @@ def juego_del_dado():
     puntusu = 0
     puntcomp = 0
     while puntusu <= 30 and puntcomp <= 30:
+        print("Presiona Enter para lanzar")
+        press=input()
         num1 = random.randint(1,6)
+        print("Tu numero fue un",num1)
         puntusu += num1
         mensaje = ''
         if puntusu >= 30:
-            mensaje = 'Ganasteee!!'
+            mensaje = 'Llegaste a 30; Ganasteee!!'
             break
         num2 = random.randint(1,6)
         puntcomp += num2
+        print("El numero de la computadora fue un",num2)
         if puntcomp >= 30:
-            mensaje = 'Noo, perdiste:(('
+            mensaje = 'Noo, la maquina llegó a 30, perdiste:(('
             break
     return mensaje
     pass
